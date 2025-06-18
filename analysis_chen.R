e698770e49d85c653c85d041420f8c346e71df8c
tombeesley <t.beesley@lancaster.ac.uk>
2025-06-18 16:21:22 +0100

    Update analysis_chen.R

--- analysis_chen.R
+++ analysis_chen.R
@@ -2,4 +2,19 @@
 
 library(tidyverse)
 
-d <- read_csv("Chen_data_tobii.csv")
+d_raw <- read_csv("Chen_data_tobii.csv")
+
+d_raw <- janitor::clean_names(d_raw)
+
+
+d <- 
+  d_raw %>% 
+  select(recording_timestamp, 
+         pID = participant_name,
+         gaze_point_x,
+         gaze_point_y,
+         eye_movement_type,
+         gaze_event_duration,
+         eye_movement_type_index,
+         fixation_point_x,
+         fixation_point_y)
